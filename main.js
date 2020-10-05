359526751ddb64791762d7a925bf264f252b450d
josepheric <61129358+josepheric@users.noreply.github.com>
2020-10-05 09:24:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function main() {
     var vertexShaderCode = `
     void main () {
         gl_Position = vec4 (0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 10.0;
+        gl_PointSize = 30.0;
     }
     `;
     var vertexShader = gl.createShader(gl.VERTEX_SHADER); //create shader
