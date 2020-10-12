c9a2991a6f3122767a40d69f28e0ff52dd58ab8f
josepheric <61129358+josepheric@users.noreply.github.com>
2020-10-12 08:39:31 +0700

    berhasil buat 3 titik
    berhasil buat 3 titik

--- main.js
+++ main.js
@@ -2,13 +2,30 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
+
+
+     // Definisi verteks-verteks pada segitiga
+  /**
+   * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5) -->ClipCoordinates
+   */
+    var vertices = [-0.5, 0.5, //titikA
+         -0.5, -0.5,  //B
+         0.5, -0.5]; //C
+    
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); //ngasih tau buffer GPU kita mau pake positionbuffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null); //setelah selesai bindnya diputus
+
+    
+    var vertexShaderCode = document.getElementById("vertexShaderCode").text;
     //Obj vertexShader
-    var vertexShaderCode = `
-    void main () {
-        gl_Position = vec4 (0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 30.0;
-    }
-    `;
+    // var vertexShaderCode = `
+    // void main () {
+    //     gl_Position = vec4 (0.0, 0.0, 0.0, 1.0);
+    //     gl_PointSize = 30.0;
+    // }
+    // `;
     var vertexShader = gl.createShader(gl.VERTEX_SHADER); //create shader
     gl.shaderSource (vertexShader, vertexShaderCode); //isiin code shadernnya
     gl.compileShader (vertexShader); //compile
@@ -29,9 +46,18 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.vertexAttribPointer(aPosition, 2,gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var primitive = gl.POINTS;
+    var offset = 0;
+    var count = 3;
 
     
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(primitive, offset, count);
 }
