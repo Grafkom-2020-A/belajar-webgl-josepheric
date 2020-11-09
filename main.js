8e28c9c22cdc539b066394ae86a74237a84bef30
josepheric <61129358+josepheric@users.noreply.github.com>
2020-11-09 13:34:52 +0700

    onclick event

--- main.js
+++ main.js
@@ -8,17 +8,18 @@ function main() {
   /**
    * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5) D (0.5,0.5)-->ClipCoordinates
    */
-    var vertices = [ -0.5, 0.5,       // Titik A 
-        -0.5, -0.5,     // Titik B
-        0.5, -0.5,      // Titik C
-        0.5, -0.5,      // Titik C
-        0.5, 0.5,       // Titik D
-        -0.5, 0.5       // Titik A 
+    var vertices = [  
+        -0.5, 0.5, 1.0, 0.0, 0.0,      // Titik A 
+        -0.5, -0.5, 1.0, 0.0, 0.0,     // Titik B
+        0.5, -0.5, 1.0, 0.0, 0.0,      // Titik C
+        0.5, -0.5, 0.0, 0.0, 1.0,      // Titik C
+        0.5, 0.5, 0.0, 0.0, 1.0,       // Titik D
+        -0.5, 0.5, 0.0, 0.0, 1.0       // Titik A 
          
         ]; 
     
-    var positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); //ngasih tau buffer GPU kita mau pake positionbuffer
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); //ngasih tau buffer GPU kita mau pake vertexBuffer
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null); //setelah selesai bindnya diputus
 
@@ -36,8 +37,11 @@ function main() {
     gl.compileShader (vertexShader); //compile
 
     //Obj fragmentShader
-    var fragmentShaderCode = `void main () {
-        gl_FragColor = vec4 (1.0, 0.0, 0.0, 1.0);
+    var fragmentShaderCode = `
+    precision mediump float;
+    varying vec3 v_Color;
+    void main () {
+        gl_FragColor = vec4(v_Color, 1.0);
     }
     `;
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER); //create
@@ -51,10 +55,25 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
-    gl.vertexAttribPointer(aPosition, 2,gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPosition);
+    var aColor = gl.getAttribLocation(shaderProgram, "a_Color");
+    gl.vertexAttribPointer(
+        aPosition, 
+        2, 
+        gl.FLOAT, 
+        false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        0);
+      gl.vertexAttribPointer(
+        aColor, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        2 * Float32Array.BYTES_PER_ELEMENT);
+      gl.enableVertexAttribArray(aPosition);
+      gl.enableVertexAttribArray(aColor);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
